e9704499751b93f76f28b1a5b01de97df707bae0
pradeep7G <pradeep7chinna@gmail.com>
2021-06-14 12:47:22 +0530

    completed ex-3.13 configured fetching of entries from mongo db,configured mongo db

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
+require('dotenv').config();
 const express=require('express');
 const morgan=require('morgan');
 const cors=require('cors');
 const app=express();
+const mongoose=require('mongoose');
+const url=process.env.MONGODB_URI;
+
+console.log('connecting to ',url);
+mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:true,useCreateIndex:true})
+.then(result =>{
+    console.log('connected to Mongo DB');
+})
+.catch(error=>{
+    console.log('error connecting mongo DB: ',error.message);
+})
+
+const personSchema=new mongoose.Schema({
+        name:String,
+        number:String,
+        date:Date,
+});
+
+personSchema.set('toJSON',{
+  transform:(document,returnedObject)=>{
+    returnedObject.id=returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+})
+
+const Person=mongoose.model('Person',personSchema);
 
 morgan.token('data',(req)=>{
     if(req.body!==undefined)
@@ -18,53 +46,44 @@ app.use(morgan('tiny',{
 app.use(morgan(':method :url :status :req[content-length] - :response-time ms :data',{
     skip:(req,res)=> req.method!=='POST'
 }));
-let persons=[
-    {
-        id:1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id:2,
-        name:"Ada Lovelace",
-        number:"39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id:4,
-        name:"Mary-Poppendick",
-        number:"39-23-6423122"
-    }
-]
+// let persons=[
+//     {
+//         id:1,
+//         name: "Arto Hellas",
+//         number: "040-123456"
+//     },
+//     {
+//         id:2,
+//         name:"Ada Lovelace",
+//         number:"39-44-5323523"
+//     },
+//     {
+//         id: 3,
+//         name: "Dan Abramov",
+//         number: "12-43-234345"
+//     },
+//     {
+//         id:4,
+//         name:"Mary-Poppendick",
+//         number:"39-23-6423122"
+//     }
+// ]
 
 app.get('/',(req,res)=>{
     res.send('<h1>Phone Book</h1>')
 })
 
 app.get('/api/persons',(req,res)=>{
-    res.json(persons);
+    Person.find({}).then((result)=>{
+        res.json(result);
+    })
 })
 
 app.get('/api/persons/:id',(req,res)=>{
-    const id=Number(req.params.id);
-    const isPresent=persons.find(p => p.id === id);
-    if(isPresent)
-    {
-        res.json(isPresent);
-    }
-    else{
-        res.status(400).send('bad request');
-    }
+   Person.findById(req.params.id).then(result => {
+       res.json(result);
+   })
 })
-
-const generateId=()=>{
-    const number=Math.floor(Math.random()*100000);
-    return number;
-}
 
 app.post('/api/persons',(req,res)=>{
     const person=req.body;//remember you have to enable/import json parser i.e app.use(express.json())
@@ -112,7 +131,7 @@ app.get('/info',(req,res)=>{
     res.send(`<div>Phonebook has info for ${persons.length} people</div> <br> ${new Date()}`)
 })
 
-const PORT=process.env.PORT || 3001;
+const PORT=process.env.PORT ;
 
 app.listen(PORT,()=>{
     console.log(`listening on port ${PORT}`);
---
+++ models/Person.js
@@ -0,0 +1,8 @@
+// const mongoose=require('mongoose');
+
+// const url=process.env.MONGODB_URI;
+// console.log('connecting to Mongo DB ',url);
+// mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:true})
+// .then(()=>{
+//     console.log('connected to Mongo DB');
+// })
