13c1db13b5f7c32c89f57b578a5cf9a1f11743d6
pradeep7G <pradeep7chinna@gmail.com>
2021-06-14 16:24:20 +0530

    changes to index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express=require('express');
 const morgan=require('morgan');
-const cors=require('cors');
+// const cors=require('cors');
 const axios=require('axios');
 const app=express();
 const mongoose=require('mongoose');
@@ -14,7 +14,7 @@ morgan.token('data',(req)=>{
 
 app.use(express.json());
 app.use(express.static('build'));
-app.use(cors());
+// app.use(cors());
 app.use(morgan('tiny',{
     skip: (req,res) => req.method==='POST'
 }));
